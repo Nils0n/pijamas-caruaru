689d2d97644f52596b3b342870b0d2781344a676
nils0n <nilson_jose.v@hotmail.com>
2021-06-24 12:48:17 -0300

    add-toogle

---
+++ assests/js/main.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('#header nav')
+const toogle = document.querySelectorAll('nav .toogle')
+
+for (const element of toogle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
